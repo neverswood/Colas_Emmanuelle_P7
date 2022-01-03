4f2ec074936765d2960d09e835c491d16fa7911b
neverswood <nevers_wood@hotmail.fr>
2022-01-03 14:24:50 +0100

    Add function searchKeyword

--- js/searchbar.js
+++ js/searchbar.js
@@ -8,3 +8,23 @@ async function searchKeyword() {
   }
 }
 searchKeyword();
+
+const containerRecipe = document.getElementById("container-recipe");
+const divRecipe = document.createElement("div");
+const templateRecipe = `
+<div class="recipe__total">
+<div class="recipe__header">
+<h3>allRecipes.indexOf("name")</h3>
+<span>time</span>
+</div>
+<div>
+<div class="recipe__ingrédients">
+<p>ingredient: </p>
+<span>quantity unit</span>
+</div>
+<div class="recipe__explanations"></div>
+</div>
+</div>
+`;
+
+console.log(allRecipes);
